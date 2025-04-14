5c0c59b2174fa57a739e8e90a8104297a7147cd5
volkantemelli <volkan.temelli@gmail.com>
2025-04-14 15:52:45 +0200

    Refactor accordion answers: update answer paragraphs to be hidden by default for improved initial visibility and adjust CSS padding for better layout consistency

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 "use strict";
 
-/* // Select and store html elements
+// Select and store html elements
 const answerParagraphs = document.querySelectorAll(".answer");
-const questions = document.querySelectorAll(".question");
 const qaSections = document.querySelectorAll(".qa-section");
 
 // Define a function to manage Accordion handler by click targets
@@ -29,4 +28,4 @@ function toggleActiveClass(element) {
 // Add click event listener to each question and give manageAccordion to it as an argument
 qaSections.forEach((element) => {
   element.addEventListener("click", manageAccordion);
-}); */
+});
