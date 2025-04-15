6edb2df7e53e27542499e4859de445b4fd585d22
volkantemelli <volkan.temelli@gmail.com>
2025-04-15 19:46:56 +0200

    Update accordion content and styling: change question titles for clarity, adjust answer visibility, and enhance active class styling for better user experience.

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
 "use strict";
 
-// Select and store html elements
-const answerParagraphs = document.querySelectorAll(".answer");
+// Select and store sections
 const qaSections = document.querySelectorAll(".qa-section");
 
 // Define a function to manage Accordion handler by click targets
 function manageAccordion(e) {
-  // If a section contains the clicked element, toggle accordion and display paragraph
-  qaSections.forEach((section, index) => {
+  // If a section contains the clicked element, toggle accordion to display paragraph
+  qaSections.forEach((section) => {
     if (section.contains(e.target)) {
-      toggleAccordion(answerParagraphs[index]);
       toggleActiveClass(section);
     }
   });
 }
 
-// Open accordion if closed, close if open by toggling class name
-function toggleAccordion(element) {
-  element.classList.toggle("hidden");
-}
-
-// Change the color of number, question and svg by adding or removing them active class
+// Change the color of number, question and svg and add/remove top border by adding or removing active class
 function toggleActiveClass(element) {
   for (let i = 0; i < 3; i++) {
-    element.children[i].classList.toggle("active");
+    element.classList.toggle("active");
   }
 }
 
